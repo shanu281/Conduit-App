3d3d3f66a67aecd151abbe7342ccd39953bdbeed
Shanu <shanukumar@shanus-MacBook-Air.local>
2023-02-06 20:05:06 +0530

    register added

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import Header from '../Header'
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import { Route, Routes } from "react-router-dom";
+import Register from "../Register";
 
 const App = () => {
   return (
     <div>
-        <Header />
+      <Header />
+      <Routes>
+        {/* <Route element={<Home />} path="/home" /> */}
+        {/* <Route element={<NewArticle />} path="/new-article" /> */}
+        {/* <Route element={<Settings />} path="/setting" /> */}
+        {/* <Route element={<SignIn />} path="/signin" /> */}
+        <Route element={<Register />} path="/register" />
+      </Routes>
+      <Footer />
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
