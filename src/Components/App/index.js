7dca08129c603f0af4912b9d381f3b7bae4277df
Shanu <shanukumar@shanus-MacBook-Air.local>
2023-02-06 22:21:50 +0530

    HTML , CSS completed

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -4,8 +4,11 @@ import Footer from "../Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home";
 import Register from "../Register";
+import SignIn from "../SignIn";
 import Settings from "../Settings";
 import NewArticle from "../NewArticle";
+
+// import Profile from "../Profile";
 
 const App = () => {
   return (
@@ -15,9 +18,10 @@ const App = () => {
         <Route element={<Home />} path="/home" />
         <Route element={<NewArticle />} path="/new-article" />
         <Route element={<Settings />} path="/settings" />
-        {/* <Route element={<SignIn />} path="/sign-in" /> */}
+        <Route element={<SignIn />} path="/sign-in" />
         <Route element={<Register />} path="/register" />
       </Routes>
+      {/* <Profile /> */}
       <Footer />
     </div>
   );
---
+++ src/Components/SignIn/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div Name="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign In </h1>
+
+            <form>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                />
+              </fieldset>
+              <button className="btn btn-lg btn-primary pull-xs-right">
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
